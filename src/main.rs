f3c8b5153d3b3738fc9be9d5246fe3ba0e1e9357
Alex Kofke <akofke@gmail.com>
2015-12-30 14:31:58 -0500

    added suit unicode characters
    Signed-off-by: Alex Kofke <akofke@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    const SPADE: char = '\u{2660}';
+    const HEART: char = '\u{2665}';
+    const DIAMOND: char = '\u{2666}';
+    const CLUB: char = '\u{2663}';
+    println!("{} {} {} {}", SPADE, HEART, DIAMOND, CLUB);
 }
